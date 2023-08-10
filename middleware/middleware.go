e0df9bf3272f3e5bf32a542c448b0f5659c2251d
rong <rong.chen@sealan.tech>
2023-08-10 19:08:21 +0800

    只有管理员“Tom”能够创建和删除单 词卡。

---
+++ middleware/middleware.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		// 验证 JWT
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return "jwtSecret", nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		// 将用户信息存储到上下文中，供后续处理函数使用
+		claims := token.Claims.(jwt.MapClaims)
+		c.Set("username", claims["username"])
+		c.Set("admin", claims["admin"])
+		c.Next()
+	}
+}
+
+func AuthModifyMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		admin, _ := c.Get("admin")
+		if admin != true {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Permission denied"})
+			c.Abort()
+			return
+		}
+	}
+}
--- router.go/routes.go
+++ router.go/routes.go
@@ -2,19 +2,21 @@ package router
 
 import (
 	"word-card-app/handlers"
+	"word-card-app/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
+	r.POST("/login", handlers.Login)
 	v1 := r.Group("/api/v1")
+	v1.Use(middleware.AuthMiddleware())
 	{
-		r.POST("/login", handlers.Login)
 
 		v1.GET("/word-cards", handlers.GetWordCards)
-		v1.POST("/word-cards", handlers.CreateWordCard)
-		v1.PUT("/word-cards/:id", handlers.UpdateWordCard)
-		v1.DELETE("/word-cards/:id", handlers.DeleteWordCard)
+		v1.POST("/word-cards", middleware.AuthModifyMiddleware(), handlers.CreateWordCard)
+		v1.PUT("/word-cards/:id", middleware.AuthModifyMiddleware(), handlers.UpdateWordCard)
+		v1.DELETE("/word-cards/:id", middleware.AuthModifyMiddleware(), handlers.DeleteWordCard)
 	}
 
 }
