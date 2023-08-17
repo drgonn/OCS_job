80587ad06dccc7f065aa150d15941e71a806d375
rong <rong.chen@sealan.tech>
2023-08-18 01:55:53 +0900

    初始化数据库连接

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"errors"
+	"fmt"
+	"word-card-app/global"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+func NewGormClient(config global.DatabaseSettingS) (*gorm.DB, error) {
+	switch config.Source {
+	case "postgres":
+		// dsn := "root:root@postgres_tenant:5432/tenant?sslmode=disable"
+		dsn := fmt.Sprintf(
+			"host=%s port=%s user=%s dbname=%s password=%s sslmode=disable TimeZone=Asia/Shanghai",
+			config.Host, config.Port, config.User, config.Dbname,
+			config.Password,
+		)
+
+		gdb, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+			Logger: logger.Default.LogMode(logger.Info),
+		},
+		)
+
+		return gdb, err
+	default:
+		return nil, errors.New("没有匹配的数据库")
+	}
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"word-card-app/db"
 	"word-card-app/global"
 	"word-card-app/router.go"
 
@@ -13,6 +14,17 @@ func main() {
 	err := global.SetupSetting()
 	if err != nil {
 		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	// fmt.Println(global.ServerSetting)
+	// fmt.Println(global.DatabaseSetting)
+	// fmt.Println(global.AppSetting)
+
+	// 第二步，初始化日志
+
+	// 第三步，初始化数据库
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
 	}
 
 	r := gin.Default()
@@ -27,4 +39,16 @@ func main() {
 	// 创建数据库表
 
 	r.Run(":38085")
+}
+
+func setupDBEngine() error {
+	var err error
+	global.GormEngine, err = db.NewGormClient(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	// model.Migrate(global.GormEngine)
+
+	return nil
 }
